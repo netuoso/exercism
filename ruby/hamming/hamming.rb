68c9caef765a7ce6236f982a9591ae3a3ee3d9a6
Andrew C (netuoso) <netuoso@pobox.com>
2015-01-29 15:54:38 -0800

    Program working correctly. Removed global variable and edited logic.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,29 +5,27 @@
 
 class Hamming
 
-# Hold 0 in a global var for number of differences
-  $count = 0
-
   def self.compute(str_1, str_2)
+
+    count = 0
 
     if str_1.length == str_2.length
 
       if str_1 == str_2
         0
-      elsif str_1.length == 1
-        1
-      elsif str_1.length >= 2
+      elsif str_1.length >= 1
+
         arr_1 = str_1.split('')
         arr_2 = str_2.split('')
 
-        until arr_1.count == 0 do
+        arr_1.count.times do
           if arr_1.pop != arr_2.pop
-            $count += 1
+            count += 1
           end
         end
 
-        $count
-        
+        count
+
       end
 
     else
