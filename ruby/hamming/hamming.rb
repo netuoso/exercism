122e25914949ae7cc274cad9ae9b5b4fa2a30869
Andrew C (netuoso) <netuoso@pobox.com>
2015-01-29 15:27:47 -0800

    Program seemingly finished, however receiving odd counts that dont make sense.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,42 @@
+#!/usr/bin/env ruby
+
 # Exercism 1
 # Hamming Test
 
 class Hamming
 
-  def self.compute(string1, string2)
-    0
+# Hold 0 in a global var for number of differences
+  $count = 0
+
+  def self.compute(str_1, str_2)
+
+    if str_1.length == str_2.length
+
+      if str_1 == str_2
+        0
+      elsif str_1.length == 1
+        1
+      elsif str_1.length >= 2
+        arr_1 = str_1.split('')
+        arr_2 = str_2.split('')
+
+        until arr_1.count == 0 do
+          if arr_1.pop != arr_2.pop
+            $count += 1
+          end
+        end
+
+        $count
+        
+      end
+
+    else
+
+      puts "You have input a sequence of improper length. Aborting."
+      0
+
+    end
+    
   end
 
 end
---
+++ ruby/hamming/test_call.rb
@@ -0,0 +1,3 @@
+require_relative 'hamming'
+
+puts Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
