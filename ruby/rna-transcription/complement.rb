2c0df589a5e18ab20b2d42d8e1962c07c71bc1e7
Andrew C (netuoso) <netuoso@pobox.com>
2015-01-30 11:01:43 -0800

    Highly optimized. Done.

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -4,37 +4,23 @@
 # RNA Transcription
 # This program computes the RNA complete to a DNA nucleotide strand
 
-# dna_to_rna
-# `C` -> `G`
-# `G` -> `C`
-# `T` -> `A`
-# `A` -> `U`
-
-# rna_to_dna
-# `G` -> `C`
-# `C` -> `G`
-# `A` -> `T`
-# `U` -> `A`
-
 class Complement
+    @dna_seq = { 'C' => 'G',
+                 'G' => 'C',
+                 'T' => 'A',
+                 'A' => 'U' }
+
+    @rna_seq = @dna_seq.invert
 
   def self.of_dna(seq)
 
-    dna_seq = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U' }
-
-    dna_comp = ''
-    seq.split('').each { |x| dna_comp += dna_seq[x].to_s }
-    dna_comp
+    seq.chars.map { |x| @dna_seq[x] }.join
 
   end
 
   def self.of_rna(seq)
 
-    rna_seq = { 'G' => 'C', 'C' => 'G', 'A' => 'T', 'U' => 'A' }
-
-    rna_comp = ''
-    seq.split('').each { |x| rna_comp += rna_seq[x].to_s }
-    rna_comp
+    seq.chars.map { |x| @rna_seq[x] }.join
 
   end
 
