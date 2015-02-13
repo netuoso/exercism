2530ce7093577b7b743eea280beb2f7b3597532f
Andrew C (netuoso) <netuoso@pobox.com>
2015-02-13 00:45:34 -0800

    Added beer-song

---
+++ ruby/beer-song/beer_song.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+# Exercism 25
+# 99 Bottles of Beer
+
+# Lyrics:
+# 99 bottles of beer on the wall, 99 bottles of beer.
+# Take one down and pass it around, 98 bottles of beer on the wall.
+# ...
+# ...
+# 1 bottle of beer on the wall, 1 bottle of beer.
+# Take it down and pass it around, no more bottles of beer on the wall.
+# No more bottles of beer on the wall, no more bottles of beer.
+# Go to the store and buy some more, 99 bottles of beer on the wall.
+
+class BeerSong
+
+  def sing
+
+  end
+
+  def verse(num)
+
+  end
+
+  def verse_end
+
+  end
+
+  def verses(from_verse, to_verse)
+
+  end
