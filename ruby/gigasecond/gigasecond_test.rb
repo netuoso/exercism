b3e4437eb26dbfe5b9b0a9ec67dedcbaeeffb8b7
Andrew C (netuoso) <netuoso@pobox.com>
2015-02-12 19:08:39 -0800

    Updating

--- ruby/gigasecond/gigasecond_test.rb
+++ ruby/gigasecond/gigasecond_test.rb
@@ -11,22 +11,26 @@ class GigasecondTest < MiniTest::Unit::TestCase
   end
 
   def test_2
+    skip
     gs = Gigasecond.from(Date.new(1977, 6, 13))
     assert_equal Date.new(2009, 2, 19), gs
   end
 
   def test_3
+    skip
     gs = Gigasecond.from(Date.new(1959, 7, 19))
     assert_equal Date.new(1991, 3, 27), gs
   end
 
   def test_time_with_seconds
+    skip
     gs = Gigasecond.from(Time.new(1959, 7, 19, 23, 59, 59))
     assert_equal Date.new(1991, 3, 28), gs
   end
 
   # modify the test to test your 1 Gs anniversary
   def test_yourself
+    skip
     your_birthday = Date.new(1989, 6, 28)
     gs = Gigasecond.from(your_birthday)
     assert_equal Date.new(2021, 3, 6), gs
