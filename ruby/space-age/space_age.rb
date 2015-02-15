529f5ad4dc72a29af73ad849764f7f500359add7
Andrew C (netuoso) <netuoso@pobox.com>
2015-02-14 18:14:56 -0800

    Done

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -14,35 +14,35 @@ class SpaceAge
   end
 
   def on_earth
-    (@age / 31557600.0).round(2)
+    (@age / 31557600.0).round_to(2)
   end
 
   def on_mercury
-    (on_earth / 0.2408467).round(2)
+    (on_earth / 0.2408467).round_to(2)
   end
 
   def on_venus
-    (on_earth.round(2) / 0.61519726).floor_to(2)
+    (on_earth / 0.61519726).floor_to(2)
   end
 
   def on_mars
-    (on_earth / 1.8808158).round(2)
+    (on_earth / 1.8808158).round_to(2)
   end
 
   def on_jupiter
-    (on_earth / 11.862615).round(2)
+    (on_earth / 11.862615).round_to(2)
   end
 
   def on_saturn
-    (on_earth / 29.447498).round(2)
+    (on_earth / 29.447498).round_to(2)
   end
 
   def on_uranus
-    (on_earth / 84.016846).round(2)
+    (on_earth / 84.016846).round_to(2)
   end
 
   def on_neptune
-    (on_earth / 164.79132).round(2)
+    (on_earth / 164.79132).round_to(2)
   end
 
 end
