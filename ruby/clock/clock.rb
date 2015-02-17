c97aab3a27fbcb1532151a158cf03830a4cc2be6
Andrew C (netuoso) <netuoso@pobox.com>
2015-02-16 20:10:15 -0800

    Clock working

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -5,8 +5,34 @@
 
 class Clock
 
-  def self.at(hours, minutes=0)
-    hours.to_f
+  def self.at(hour, minutes=0)
+    new(hour, minutes)
+  end
+
+  def self.from(time)
+    new(time.hour, time.min)
+  end
+
+  attr_reader :time
+
+  def initialize(hour, minutes)
+    @time = Time.utc(1970, 1, 1, hour, minutes)
+  end
+
+  def +(minutes)
+    self.class.from(time + (minutes*60))
+  end
+
+  def -(minutes)
+    self.class.from(time - (minutes*60))
+  end
+
+  def ==(other)
+    to_s == other.to_s
+  end
+
+  def to_s
+    time.strftime('%H:%M')
   end
 
 end
